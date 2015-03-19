63249b6cd2c734afe9eb29e78dc8d31c46a819a6
Leandro Mendes <oleandromendes@outlook.com>
2015-03-19 16:10:42 +0000

    New Camera Scripts & Prefabs
    New Camera Scripts & Prefabs

---
+++ Assets/Scripts/CameraScripts/CameraMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+	public float moveSpeed = 15;
+	public float edgeGap = 5;
+	public CameraZoom camera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		handleMovement();
+		handleCameraPosition();
+	}
+
+	void handleMovement() {
+		if (Input.GetKey(KeyCode.W)) {
+			transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+		} if (Input.GetKey(KeyCode.S)) {
+			transform.position += Vector3.back * moveSpeed * Time.deltaTime;
+		} if (Input.GetKey(KeyCode.A)) {
+			transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+		} if (Input.GetKey(KeyCode.D)) {
+			transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+		} 
+	}
+
+	void handleCameraPosition() {
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, Vector3.down, out hit)) {
+			camera.updateCamera(hit.point);
+		}
+	}
+}
---
+++ Assets/Scripts/CameraScripts/CameraZoom.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZoom : MonoBehaviour {
+	public float baseHeight = 15;
+	public float minHeight = 10;
+	public float maxHeight = 30;
+
+	float steps;
+	Camera camera;
+	float zoom;
+	Vector3 lastTarget;
+
+	// Use this for initialization
+	void Start () {
+		steps = (maxHeight - minHeight) * 0.05f;
+		camera = (Camera)this.GetComponent("Camera");
+		zoom = baseHeight;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		handleZoom();
+	}
+
+	void handleZoom() {
+		float scroll = Input.GetAxis("Mouse ScrollWheel");
+		if (scroll > 0 && zoom > minHeight) {
+			zoom -= steps;
+		} else if (scroll < 0 && zoom < maxHeight) {
+			zoom += steps;
+		}
+	}
+
+	public void updateCamera(Vector3 target) {
+		transform.position = target + Vector3.up * zoom + Vector3.back * zoom;
+		transform.LookAt(target);
+	}
+}
